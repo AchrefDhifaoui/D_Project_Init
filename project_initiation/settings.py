68d1f6b9acd5453629031b06486328a7ec4f8b11
dhifaoui achref <achref.dhifaoui00@gmail.com>
2024-02-08 20:30:51 +0100

    making an app with name yassinodev and make a models

--- project_initiation/settings.py
+++ project_initiation/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'yassino_dev.apps.YassinoDevConfig'
 ]
 
 MIDDLEWARE = [
---
+++ yassino_dev/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class YassinoDevConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'yassino_dev'
